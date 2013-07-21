c2847220a1ef25dc3e76a7e001c949ce7156cb32
Andrew Barchuk <andrew@raindev.pp.ua>
2013-07-22 02:25:56 +0300

    Verbatim string is missed in Java, so regex way is bullshit. Used Apache Commons Lang for String escaping.

--- FirstQuine/src/pp/ua/raindev/Quine.java
+++ FirstQuine/src/pp/ua/raindev/Quine.java
@@ -1,15 +1,19 @@
 package pp.ua.raindev;
 
+import org.apache.commons.lang.StringEscapeUtils;
+
+import java.util.regex.Matcher;
+
 /**
- * The simplest quine, implemented in Java.
+ * Quine, implemented in Java.
  *
  * @author raindev
  */
 public class Quine {
-    private static final String source = "package pp.ua.raindev;\n\n/**\n * My first quine, implemented in Java.\n */\npublic class Quine {\n    private static final String source = \"code\";\n\n    public static void main(String[] args) {\n        System.out.println(source.replace(\"code\", source));\n    }\n\n}\n";
+    private static final String self = "package pp.ua.raindev;\n\n/**\n * Quine, implemented in Java.\n *\n * @author raindev\n */\npublic class Quine {\n    private static final String self = \"~\";\n\n    public static void main(String[] args) {\n         System.out.println(self.replaceFirst(\"~\", Matcher.quoteReplacement(StringEscapeUtils.escapeJava(self))));\n    }\n\n}";
 
     public static void main(String[] args) {
-        System.out.println(source.replaceFirst("code", source.replace("\n", "\\\\n").replace("\"code\"", "\\\\\"code\\\\\"")));
+        System.out.println(self.replaceFirst("~", Matcher.quoteReplacement(StringEscapeUtils.escapeJava(self))));
     }
 
 }
