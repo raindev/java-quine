eb95ac2c56ef9d44fb422aad31d2b07adcf5cd16
Andrew Barchuk <andrew@raindev.pp.ua>
2013-07-25 12:35:26 +0300

    Apache Commons dependency removed, while keeping single string. Now it's pure Java quine. P.S. Yeah, I know that 1,5K character long string looks terrible, I'll fix it in the future ;)

--- FirstQuine/src/pp/ua/raindev/Quine.java
+++ FirstQuine/src/pp/ua/raindev/Quine.java
@@ -1,19 +1,19 @@
 package pp.ua.raindev;
 
-import org.apache.commons.lang.StringEscapeUtils;
-
 import java.util.regex.Matcher;
 
 /**
- * Quine, implemented in Java.
+ * Quine, implemented in pure Java.
  *
  * @author raindev
  */
 public class Quine {
-    private static final String self = "package pp.ua.raindev;\n\n/**\n * Quine, implemented in Java.\n *\n * @author raindev\n */\npublic class Quine {\n    private static final String self = \"~\";\n\n    public static void main(String[] args) {\n         System.out.println(self.replaceFirst(\"~\", Matcher.quoteReplacement(StringEscapeUtils.escapeJava(self))));\n    }\n\n}";
+    private static final String self = "package pp.ua.raindev;\n\nimport java.util.regex.Matcher;\n\n/**\n * Quine, implemented in pure Java.\n *\n * @author raindev\n */\npublic class Quine {\n    private static final String self = \"package pp.ua.raindev;\\n\\nimport java.util.regex.Matcher;\\n\\n/**\\n * Quine, implemented in Java.\\n *\\n * @author raindev\\n */\\npublic class Quine {\\n    private static final String self = \\\"package pp.ua.raindev;\\\\n\\\\nimport java.util.regex.Matcher;\\\\n\\\\n/**\\\\n * Quine, implemented in Java.\\\\n *\\\\n * @author raindev\\\\n */\\\\npublic class Quine {\\\\n    private static final String self = \\\\\\\"package pp.ua.raindev;\\\\\\\\n\\\\\\\\n/**\\\\\\\\n * Quine, implemented in Java.\\\\\\\\n *\\\\\\\\n * @author raindev\\\\\\\\n */\\\\\\\\npublic class Quine {\\\\\\\\n    private static final String self = \\\\\\\\\\\\\\\"~\\\\\\\\\\\\\\\";\\\\\\\\n\\\\\\\\n    public static void main(String[] args) {\\\\\\\\n         System.out.println(self.replaceFirst(\\\\\\\\\\\\\\\"~\\\\\\\\\\\\\\\", Matcher.quoteReplacement(StringEscapeUtils.escapeJava(self))));\\\\\\\\n    }\\\\\\\\n\\\\\\\\n}\\\\\\\";\\\\n\\\\n    public static void main(String[] args) {\\\\n        System.out.println(self.replaceFirst(\\\\\\\"~\\\\\\\", Matcher.quoteReplacement(\\\\n                self.replace(String.valueOf((char) 10), (char) 92 + \\\\\\\"n\\\\\\\")\\\\n                        .replace(String.valueOf((char) 34), (char) 92 + \\\\\\\"\\\\\\\" + (char) 34))));\\\\n    }\\\\n\\\\n}\\\";\\n\\n    public static void main(String[] args) {\\n        System.out.println(self.replaceFirst(\\\"~\\\", Matcher.quoteReplacement(\\n                self.replace(String.valueOf((char) 10), (char) 92 + \\\"n\\\")\\n                        .replace(String.valueOf((char) 34), (char) 92 + \\\"\\\" + (char) 34))));\\n    }\\n\\n}\";\n\n    public static void main(String[] args) {\n        System.out.println(self.replaceFirst(\"~\", Matcher.quoteReplacement(\n                self.replace((char) 10 + \"\", (char) 92 + \"n\")\n                        .replace((char) 34 + \"\", (char) 92 + \"\" + (char) 34))));\n    }\n\n}";
 
     public static void main(String[] args) {
-        System.out.println(self.replaceFirst("~", Matcher.quoteReplacement(StringEscapeUtils.escapeJava(self))));
+        System.out.println(self.replaceFirst("~", Matcher.quoteReplacement(
+                self.replace((char) 10 + "", (char) 92 + "n")
+                        .replace((char) 34 + "", (char) 92 + "" + (char) 34))));
     }
 
 }
